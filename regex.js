75d50600d4c2bad5a1f682db7da8b019ba4399cc
ramteja-ops <tejamidatada1234@gmail.com>
2026-06-09 15:32:33 +0530

    worked on regular Expressions

---
+++ regex.js
@@ -0,0 +1,7 @@
+// let emailRegEx=/^\w{4,}(@)[a-z]{1,8}\.(com)$/
+
+// console.log(emailRegEx.test("raju_dog@jilebi.com"));
+
+let password=/[A-Z]{1,}[a-z]{1,}[0-9]{1,}[@#$&*]{1,}{8,12}/
+
+console.log(password.test("Ttj1234#"));
